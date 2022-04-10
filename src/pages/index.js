c4858df54584c1b8febe258394de42da6a5ac2d4
SmthOfValue <bik00014@gmail.com>
2022-04-10 20:26:13 +0300

    added buttons and js functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,26 @@
 import '../pages/index.css';
+import aspirantImage from '../images/study/Aspirant.png';
+import magistrImage from '../images/study/Magistr.png';
+
+const aspiranturaBtn = document.querySelector('#aspirantura');
+const magistraturaBtn = document.querySelector('#magistratura');
+const educationImage = document.querySelector('.education__image');
+magistraturaBtn.classList.toggle('education__button_active');
+
+function activateEducationButton(e) {
+    if (e.target === aspiranturaBtn) {
+        aspiranturaBtn.setAttribute("disabled", "");
+        magistraturaBtn.removeAttribute("disabled", "");
+        educationImage.src = aspirantImage;
+    }
+    else {
+        magistraturaBtn.setAttribute("disabled", "");
+        aspiranturaBtn.removeAttribute("disabled", "");
+        educationImage.src = magistrImage;
+    }
+    aspiranturaBtn.classList.toggle('education__button_active');
+    magistraturaBtn.classList.toggle('education__button_active');
+}
+
+aspiranturaBtn.addEventListener('click', activateEducationButton);
+magistraturaBtn.addEventListener('click', activateEducationButton);
